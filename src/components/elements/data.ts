646a775d9cac49cdd4131564e347284e45a618bf
mengiefen <mengiefen@gmail.com>
2023-01-24 16:37:05 +0300

    add data.ts

---
+++ src/components/elements/data.ts
@@ -0,0 +1,32 @@
+import SVG1 from '../../assets/svg1.svg';
+import SVG2 from '../../assets/svg2.svg';
+import SVG3 from '../../assets/svg3.svg';
+import SVG4 from '../../assets/svg4.svg';
+
+export const data = [
+  {
+    id: 1,
+    title: 'Safe Transaction',
+    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: SVG1,
+  },
+  {
+    id: 2,
+    title: 'Easy to use',
+    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: SVG2,
+  },
+
+  {
+    id: 3,
+    title: 'MultiCrypto',
+    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: SVG3,
+  },
+  {
+    id: 4,
+    title: 'Refundable',
+    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: SVG4,
+  },
+];
