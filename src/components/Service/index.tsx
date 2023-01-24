eb9a70b594f1a89db18119e348d915d19bd72051
mengiefen <mengiefen@gmail.com>
2023-01-24 23:16:17 +0300

    modify service section

--- src/components/Service/index.tsx
+++ src/components/Service/index.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import Button from '../elements/Button';
-import Image from 'next/image';
-import CarouselItem from '../elements/CarouselItem';
-import Image1 from '../../assets/1.jpg';
-import Carousel from '../elements/Carousel';
+import React from "react"
+import Button from "../elements/Button"
+import Image from "next/image"
+import CarouselItem from "../elements/CarouselItem"
+import Image1 from "../../assets/1.jpg"
+import Carousel from "../elements/Carousel"
 
 const Service = () => {
   return (
-    <div className="flex flex-col gap-3 h-screen">
+    <div className="flex flex-col gap-3 min-h[50%] mb-12">
+      <h2 className="text-2xl text-center text-text mt-5 tracking-wide leading-10">Services</h2>
       <Carousel />
-
-      {/* <Button
-        variant="primary"
-        label="Connect Wallet"
-        size="large"
-        onClick={() => alert('Wow, You did it!')}
-      /> */}
     </div>
-  );
-};
+  )
+}
 
-export default Service;
+export default Service
