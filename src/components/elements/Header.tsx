ec74e880594ed15493d80e7f1353648de083ccaf
mengiefen <mengiefen@gmail.com>
2023-01-24 23:16:39 +0300

    add navbar

--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Image from "next/image"
+import React from "react"
+import { FiMenu } from "react-icons/fi"
+import { GiTwoCoins } from "react-icons/gi"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className="w-screen h-[70px] flex flex-row justify-between items-center bg-slate-800 px-5 border-b-[0.5px] border-bg">
+      <div className="flex flex-row items-center gap-1">
+        <GiTwoCoins className="text-3xl" />
+        <h1 className="text-2xl text-text font-semibold tracking-wide">
+          Trust<span className="text-secondary-500 font-bold">Me</span>
+        </h1>
+      </div>
+
+      <button type="button" className="outline-none border-none p-0">
+        <FiMenu className="text-text text-3xl" />
+      </button>
+    </nav>
   )
 }
 
