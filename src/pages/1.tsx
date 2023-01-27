c05583090ca7303d34156923a4ea9e3417bff9a5
Rushikesh Jadhav <hellorushihere@gmail.com>
2023-01-27 19:25:17 +0530

    Updated 1 tx

--- src/pages/1.tsx
+++ src/pages/1.tsx
@@ -1,10 +1,15 @@
 import TransactionList from "@/components/TransactionList"
 import React from "react"
+import CreateTransaction from "@/components/elements/CreateTransaction"
 
 
 const List = () => {
-  return <TransactionList />
-  //
+  return (
+  <>
+    <TransactionList />
+  </>
+  )
+
 }
 
 export default List
