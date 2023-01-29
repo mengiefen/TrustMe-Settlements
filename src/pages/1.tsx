bb7688c7b54de824235e7c9e712f11216c7bf608
Rushikesh Jadhav <hellorushihere@gmail.com>
2023-01-29 11:19:04 +0530

    .

--- src/pages/1.tsx
+++ src/pages/1.tsx
@@ -1,6 +1,5 @@
 import TransactionList from "@/components/TransactionList"
 import React from "react"
-<<<<<<< HEAD
 import TableRow from "@/components/TransactionList/TableRow"
 import TradeList from "@/components/TransactionList/TradeList"
 
@@ -9,19 +8,5 @@ const List = () => {
     <>
       <TransactionList />
     </>
-  )
-=======
-import CreateTransaction from "@/components/elements/CreateTransaction"
-
-
-const List = () => {
-  return (
-  <>
-    <TransactionList />
-  </>
-  )
-
->>>>>>> c05583090ca7303d34156923a4ea9e3417bff9a5
-}
-
+  )}
 export default List
