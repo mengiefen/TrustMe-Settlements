bc4437573defc76a64d841920741ab17c85fb8e3
mengiefen <mengiefen@gmail.com>
2023-01-29 13:12:13 +0300

    refactor helper files

--- src/helpers/writeHelpers.ts
+++ src/helpers/writeHelpers.ts
@@ -1,81 +1,16 @@
-import { formatEther, parseEther } from "ethers/lib/utils"
-import { Alchemy, Network } from "alchemy-sdk"
+import { trustMeContract } from "../constants/interact"
 
-import {Trust_Me_Contract} from '../constants/interact';
-
-const ALECHEMY_API_KEY = process.env.ALCHEMY_GOERLI_API_KEY
-
-import { ethers } from "ethers"
-import { CONTRACT_ABI } from "./../constants/interact"
-
-const ALCHEMY_API_KEY = process.env.ALCHEMY_GOERLI_API_KEY || ""
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || ""
-const PRIVATE_KEY = process.env.PRIVATE_KEY_ONE || ""
-
-
-const settings = {
-  apiKey: ALCHEMY_API_KEY,
-  network: Network.ETH_GOERLI,
-}
-
-const alchemy = new Alchemy(settings)
-
-// Provider
-const provider = new ethers.providers.AlchemyProvider("goerli", ALCHEMY_API_KEY)
-
-// Signer
-const signer = new ethers.Wallet(PRIVATE_KEY, provider)
-
-// Contract
-const contract = new ethers.Contract(CONTRACT_ADDRESS, JSON.stringify(CONTRACT_ABI), signer)
-
-
-
-async function getPendingTrades() {
-  const pendingTradesIDs = await Trust_Me_Contract.methods.getPendingTradesIDs().call();
-  return pendingTradesIDs;
-}
-
-async function getTrade(tradeId:number) {
-  const trade = await Trust_Me_Contract.methods.getTrade(tradeId).call();
-  return trade;
-}
-
-async function getTradeIdToTrade(tradeId:number){
-  return await Trust_Me_Contract.methods.getTradeIdToTrade(tradeId).call();
-}
-
-async function confirmTrade(tradeId:number){
-  return await Trust_Me_Contract.methods.confirmTrade(tradeId);
+async function confirmTrade(tradeId: number) {
+  return await trustMeContract.methods.confirmTrade(tradeId)
 }
 
 async function checkExpiredTrades() {
-  return await Trust_Me_Contract.methods.checkExpiredTrades();
+  return await trustMeContract.methods.checkExpiredTrades()
 }
 
-async function withdraw(tradeId:number) {
-  return await Trust_Me_Contract.methods.withdraw(tradeId);
+async function withdraw(tradeId: number) {
+  return await trustMeContract.methods.withdraw(tradeId)
 }
-// addTrade(address,address,address,uint256,uint256,uint256)
-// {addTrade(address,address,address,uint256,uint256,uint256): ƒ, cancelTrade(uint256): ƒ, checkExpiredTrades(): ƒ, confirmTrade(uint256): ƒ, getPendingTradesIDs(): ƒ, …}
-// cancelTrade(uint256)
-// checkExpiredTrades()
-// confirmTrade(uint256)
-// {addTrade(address,address,address,uint256,uint256,uint256): ƒ, cancelTrade(uint256): ƒ, checkExpiredTrades(): ƒ, confirmTrade(uint256): ƒ, getPendingTradesIDs(): ƒ, …}
-// {TradeCanceled(uint256,address,address): ƒ, TradeConfirmed(uint256,address,address): ƒ, TradeCreated(uint256,address,address): ƒ, TradeExpired(uint256,address,address): ƒ, TradeWithdrawn(uint256,address,address): ƒ, …}
-// {addTrade(address,address,address,uint256,uint256,uint256): ƒ, cancelTrade(uint256): ƒ, checkExpiredTrades(): ƒ, confirmTrade(uint256): ƒ, getPendingTradesIDs(): ƒ, …}
-// getPendingTradesIDs()
-// getTrade(uint256)
-// Interface {fragments: Array(28), _abiCoder: AbiCoder, functions: {…}, errors: {…}, events: {…}, …}
-// pendingTradesIDs(uint256)
-// {addTrade(address,address,address,uint256,uint256,uint256): ƒ, cancelTrade(uint256): ƒ, checkExpiredTrades(): ƒ, confirmTrade(uint256): ƒ, getPendingTradesIDs(): ƒ, …}
-// AlchemyProvider {_isProvider: true, _events: Array(0), _emitted: {…}, disableCcipRead: false, formatter: Formatter, …}
-// resolvedAddress
-// Promise {<rejected>: Error: resolver or addr is not configured for ENS name (argument="name", value="", code=INVALID_ARG…}
-// signer
-// tradeIDToTrade(uint256)
-
-// withdraw(uint256)
 
 export const addTrade = async (
   buyerAddress: string,
@@ -85,7 +20,7 @@ export const addTrade = async (
   amountOfTokenToBuy: number,
   deadline: number
 ) => {
-  const tx = await contract.addTrade(
+  const tx = await trustMeContract.addTrade(
     buyerAddress,
     tokenToSell,
     tokenToBuy,
@@ -100,8 +35,7 @@ export const addTrade = async (
 }
 
 export const cancelTrade = async (tradeID: number) => {
-  const tx = await contract.cancelTrade(tradeID)
+  const tx = await trustMeContract.cancelTrade(tradeID)
   tx.wait()
   return tx
 }
-
