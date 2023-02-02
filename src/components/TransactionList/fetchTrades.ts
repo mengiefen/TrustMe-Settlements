05a4aba998ee102b5820bd9058b8557748de8c26
mengiefen <mengiefen@gmail.com>
2023-02-02 12:28:33 +0300

    update fetchTrades to compare data

--- src/components/TransactionList/fetchTrades.ts
+++ src/components/TransactionList/fetchTrades.ts
@@ -4,26 +4,38 @@ import { BigNumber } from "ethers"
 import { Trade } from "./type"
 import { getTradesIDsByUser, getTrade } from "@/helpers/getterHelpers"
 
-export const getTradeList = async (amount: number, address: `0x${string}` | undefined) => {
+export const getTradeList = async (address: `0x${string}` | undefined) => {
   const tradeIds = await getTradesIDsByUser(address as string)
+  const trades: Trade[] = await getTradesFromStorage()
+  console.log("tradeIds", tradeIds.length)
+  console.log("trades", trades.length)
 
-  const trades: Trade[] = []
-  await Promise.all(
-    tradeIds.slice(0, amount).map(async (tradeId: BigNumber) => {
-      const trade = await getTrade(Number(tradeId._hex))
-      await trades.push({
-        id: Number(trade.id),
-        seller: trade.seller,
-        buyer: trade.buyer,
-        tokenToSell: await getSymbol(trade.tokenToSell),
-        tokenToBuy: await getSymbol(trade.tokenToBuy),
-        amountOfTokenToSell: formatEther(trade.amountOfTokenToSell),
-        amountOfTokenToBuy: formatEther(trade.amountOfTokenToBuy),
-        deadline: Number(trade.deadline),
-        status: getStatus(trade.status),
+  if (tradeIds.length !== trades.length) {
+    await Promise.all(
+      tradeIds.slice(trades.length, tradeIds.length).map(async (tradeId: BigNumber) => {
+        const trade = await getTrade(Number(tradeId._hex))
+        await trades.push({
+          id: Number(trade.id),
+          seller: trade.seller,
+          buyer: trade.buyer,
+          tokenToSell: await getSymbol(trade.tokenToSell),
+          tokenToBuy: await getSymbol(trade.tokenToBuy),
+          amountOfTokenToSell: formatEther(trade.amountOfTokenToSell),
+          amountOfTokenToBuy: formatEther(trade.amountOfTokenToBuy),
+          deadline: Number(trade.deadline),
+          status: getStatus(trade.status),
+        })
       })
-    })
-  )
+    )
+  }
+
+  return trades
+}
+
+export const getTradesFromStorage = async () => {
+  const trades = JSON.parse(
+    JSON.parse(localStorage.getItem("persist:trustMe") || "[]").trades
+  ).trades
 
   return trades
 }
